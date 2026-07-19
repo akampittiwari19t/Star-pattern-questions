0b276f1f08fae83dabcfa5490deaa1659f9d1da3
Akampit Deo Tiwari <at943163@gmail.com>
2026-07-20 05:19:01 +0530

    Add files via upload

---
+++ july2_q2.cpp
@@ -0,0 +1,13 @@
+ #include<iostream>
+ using namespace std;
+ int main(){
+    int a,b;
+    int num=1;
+    for(a=1;a<=4;a++){
+        for(b=1;b<=a;b++)
+        cout<<num++;
+        cout<<endl;
+
+
+    }
+ }
