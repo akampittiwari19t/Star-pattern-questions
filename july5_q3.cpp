b201e20107574f65689096a125d267737d7067b7
Akampit Deo Tiwari <at943163@gmail.com>
2026-07-20 23:00:39 +0530

    Add files via upload

---
+++ july5_q3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    for(a=1;a<=5;a++){
+        for(c=a;c<=4;c++)
+        cout<<" ";
+        for(b=1;b<=a*2-1;b++)
+        cout<<"*";
+        cout<<endl;
+    }
+     for(a=4;a>=1;a--){
+        for(c=a;c<=4;c++)
+        cout<<" ";
+        for(b=1;b<=a*2-1;b++)
+        cout<<"*";
+        cout<<endl;
+     }}
